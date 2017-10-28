95e9f756d1c05024f8456e076d3aaa0ed697bbf6
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-10-28 12:11:17 -0300

    Added movement and collision

---
+++ Assets/Scripts/CellMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(0.1f, 0.5f), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    public bool isCure;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move;
+        if (isCure)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal_Cure"), Input.GetAxis("Vertical_Cure"), 0);
+        }
+        else
+        {
+            move = new Vector3(Input.GetAxis("Horizontal_Virus"), Input.GetAxis("Vertical_Virus"), 0);
+        }
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
